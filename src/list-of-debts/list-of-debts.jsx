b57a50331e003f2c6cb2acb0d584f83b9b9dcebe
andrius.kripaitis <andrius.kripaitis@gmail.com>
2017-11-07 14:01:10 +0200

    Use table for list visualisation;

--- src/list-of-debts/list-of-debts.jsx
+++ src/list-of-debts/list-of-debts.jsx
@@ -3,23 +3,36 @@ import React from 'react';
 export default class ListOfDebts extends React.Component {
   render() {
     return (
-      <ul>
-        <span>{this.props.value.map(item =>
-          <li key={item.id}>
-            {item.name}
-            <span className="controls">
-              <span
-                onClick={() => this.props.showEditForm(item)}
-              >Edit</span>
-              |
-              <span
-                onClick={() => this.props.showDeleteForm(item)}
-              >Delete</span>
-            </span>
-          </li>
+      <table>
+        <thead>
+          <tr>
+            <td>Returned</td>
+            <td>Name</td>
+            <td>Date borrowed</td>
+            <td>Due date</td>
+            <td></td>
+          </tr>
+        </thead>
+        <tbody>
+          {this.props.value.map(item =>
+            <tr key={item.id}>
+              <td>{item.isReturned}</td>
+              <td>{item.name}</td>
+              <td>{item.dateBorrowed}</td>
+              <td>{item.dueDate}</td>
+              <td className="controls">
+                <span
+                  onClick={() => this.props.showEditForm(item)}
+                >Edit</span>
+                |
+                <span
+                  onClick={() => this.props.showDeleteForm(item)}
+                >Delete</span>
+              </td>
+            </tr>
           )}
-          </span>
-      </ul>
+        </tbody>
+      </table>
     );
   }
 }
